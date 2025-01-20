a907ca6954b1667732f7aa2ad1bbbecd8b54b74e
Rishi-Jain2602 <rj1016743@gmail.com>
2025-01-21 03:43:34 +0530

    fix server issue

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const cors = require('cors'); 
 require('dotenv').config();
 
 app.use(cors({
     origin: 'https://sentiment-analysis-insta-post-frontend.vercel.app'
   }));
 
+  
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
