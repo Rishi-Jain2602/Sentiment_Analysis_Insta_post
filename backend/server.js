421ec68156bd2dba43409f59fb777cca1ecd4dd9
Rishi-Jain2602 <rj1016743@gmail.com>
2025-01-21 15:14:38 +0530

    add cors in server

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,21 @@ const port = 5000;
 const cors = require('cors'); 
 require('dotenv').config();
 
-// app.use(cors({
-//     origin: 'https://sentiment-analysis-insta-post-frontend.vercel.app'
-//   }));
+app.use(cors({
+    origin: 'https://sentiment-analysis-insta-post-frontend.vercel.app'
+  }));
 
   
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Option, Authorization, X-Requested-With,Origin,Accept');
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Option, Authorization, X-Requested-With,Origin,Accept');
 
-    if (req.method === 'OPTIONS') {
-        return res.status(200).json({});
-    }
-    next();
-});
+//     if (req.method === 'OPTIONS') {
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
 app.use(express.json()); 
 app.use('/db',require('./Routes/fetch'));
