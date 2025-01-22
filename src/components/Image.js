93bbf02038f28f0b76ae3f0d972f5d50cca39ee8
Rishi-Jain2602 <rj1016743@gmail.com>
2025-01-23 02:38:57 +0530

    added Images, video,counting etc in the UI

--- src/components/Image.js
+++ src/components/Image.js
@@ -7,9 +7,10 @@ export default function Image() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.post('https://sentiment-analysis-insta-post.vercel.app/db/fetchImg')
+    // axios.post('https://sentiment-analysis-insta-post.vercel.app/db/fetchImg')
+    axios.post('http://localhost:5000/db/fetchImg')
       .then(response => {
-        setData(response.data); 
+        setData(response.data);
       })
       .catch(error => {
         console.error("There was an error fetching the data!", error);
@@ -21,26 +22,54 @@ export default function Image() {
 
   return (
     <div className="container">
-      {data.map((item) => (
+      {data.map((item,index) => (
         <div key={item._id} className="card mb-4" >
-          <img src={item.dislayurl} className="card-img-top" alt={"Not Able to fetch the Image"} />
-          
-        
+          <div className="count-badge">{index + 1}</div>
+          <img src={`data:image/jpeg;base64,${item.image_data}`} className="card-img-top" alt={"Not Able to fetch the Image"} />
+
+
           <div className="card-body">
+
             <h5 className="card-title">{item.ownerFullName}</h5>
             <p className="card-text">{item.caption}</p>
             <p className="card-text"><strong>Tajness Score:</strong> {item["Tajness Score"]}</p>
+            <p className="card-text"><strong>Score Explanation</strong>
+              {item['score_explanation']}
+            </p>
             <p className="card-text"><strong>Permission:</strong> {item.Permission ? 'Granted' : 'Not Granted'}</p>
             <p className="card-text"><strong>Reason:</strong> {item.Reason || 'No reason provided'}</p>
             <p className="card-text"><strong>BioGraphy:</strong> {item.BioGraphy}</p>
-            <p className="card-text"><strong>Description:</strong> {item["Data Collection"].description}</p>
-            <p className="card-text"><strong>Identified Hotel:</strong> {item["Data Collection"].IdentifiedHotel ? 'Yes' : 'No'}</p>
-            <p className="card-text"><strong>Hotel Name:</strong> {item["Data Collection"].HotelName || 'No Hotel Name is Available'}</p>
-            <p className="card-text"><strong>Relation:</strong> {item["Data Collection"].Relation}</p>
-            <p className="card-text"><strong>Inhouse/Wild:</strong> {item["Data Collection"].Inhouse_Wild ? 'Yes' : 'No'}</p>
+            <p className="card-text"><strong>Description:</strong>
+              {item["Data Collection"] && item["Data Collection"].description
+                ? item["Data Collection"].description
+                : "No Description provided"}
+            </p>
+            <p className="card-text"><strong>Identified Hotel:</strong>
+              {item["Data Collection"] && item["Data Collection"].IdentifiedHotel
+                ? 'Yes'
+                : 'No'}
+            </p>
+            <p className="card-text"><strong>Hotel Name:</strong>
+              {item["Data Collection"] && item["Data Collection"].HotelName
+                ? item["Data Collection"].HotelName
+                : 'No Hotel Name is Available'}
+            </p>
+            <p className="card-text"><strong>Relation:</strong>
+              {item["Data Collection"] && item["Data Collection"].Relation
+                ? item["Data Collection"].Relation
+                : 'No Relation available'}
+            </p>
+            <p className="card-text"><strong>Inhouse/Wild:</strong>
+              {item["Data Collection"] && item["Data Collection"].Inhouse_Wild
+                ? 'Yes'
+                : 'No'}
+            </p>
+            <p className="card-text"><strong>Categories</strong>
+              {item['Categories']}
+            </p>
 
             <p className="card-text"><strong>Hashtags:</strong> {item.hashtag.join(', ') || 'No Hashtags are there'}</p>
-            <Link to={item.url}target="_blank" rel="noopener noreferrer" className="btn btn-primary">View on Instagram</Link>
+            <Link to={item.url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">View on Instagram</Link>
           </div>
         </div>
       ))}
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './Styles/Video.css'
 import { Link } from 'react-router-dom'
@@ -8,22 +8,23 @@ export default function Video() {
   const [expandedCaption, setExpandedCaption] = useState(null);
   const [expandedFrameDesc, setExpandedFrameDesc] = useState(null);
   const [expandedOralTranscript, setExpandedOralTranscript] = useState(null);
-  
+
   const toggleCaption = (id) => {
     setExpandedCaption(expandedCaption === id ? null : id);
   };
-  
+
   const toggleFrameDesc = (id) => {
     setExpandedFrameDesc(expandedFrameDesc === id ? null : id);
   };
-  
+
   const toggleOralTranscript = (id) => {
     setExpandedOralTranscript(expandedOralTranscript === id ? null : id);
   };
-  
+
 
   useEffect(() => {
-    axios.post('https://sentiment-analysis-insta-post.vercel.app/db/fetchVid')
+    // axios.post('https://sentiment-analysis-insta-post.vercel.app/db/fetchVid')
+    axios.post('http://localhost:5000/db/fetchVid')
       .then(response => {
         setData(response.data);
       })
@@ -34,14 +35,25 @@ export default function Video() {
   }, []);
   return (
     <div className="container">
-      {data.map((item) => (
+      {data.map((item,index) => (
         <div key={item._id} className="card mb-4" >
+          <div className="count-badge">{index + 1}</div>
           <div className="card-body">
             <h5 className="card-title">{item.Result.ownerFullName}</h5>
+            {item.VideoUrl ? ( // Check if VideoUrl exists before rendering
+              <div className="video-container">
+                <video controls className="video-player">
+                  <source src={item.VideoUrl} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            ) : (
+              <p>No video available</p> // Display if VideoUrl is missing
+            )}
             <p className="card-text">{item.caption}</p>
-            <p className="card-text"><strong>BioGraphy:</strong> {item['Data_Collection']['Biography of User']}</p>
+            <p className="card-text"><strong>BioGraphy:</strong> {item['Data_Collection']['Biography of User'] || 'Nothing to show'}</p>
             {/* <p className="card-text"><strong>Caption:</strong> {item["Data_Collection"]['Caption']}</p> */}
-            
+
             {/* Caption */}
             <p className="card-text">
               <strong>Caption:</strong>
@@ -61,7 +73,7 @@ export default function Video() {
             {/* Frame Description */}
             <p className="card-text py-2">
               <strong>Frame Description:</strong>
-              {expandedFrameDesc!== item._id ? (
+              {expandedFrameDesc !== item._id ? (
                 <span className="truncate">
                   {item["Data_Collection"]['Frame Description']}
                 </span>
@@ -74,7 +86,7 @@ export default function Video() {
             <Link to="#!" onClick={() => toggleFrameDesc(item._id)}>
               {expandedFrameDesc === item._id ? 'Read Less' : 'Read More'}
             </Link>
-            
+
             {/* Oral Transcript */}
             <p className="card-text py-2">
               <strong>Oral Transcript:</strong>
@@ -94,15 +106,21 @@ export default function Video() {
 
             {/* <p className="card-text py-2"><strong>Oral Transcript:</strong> {item["Data_Collection"]['Oral Transcript']}</p> */}
             <p className="card-text"><strong>Identified Hotel:</strong> {item["Summary"].IdentifiedHotel ? 'Yes' : 'No'}</p>
-              <p className="card-text"><strong>Tajness Score:</strong> {item["TajNess Score"]}</p>
+            <p className="card-text"><strong>Tajness Score:</strong> {item["TajNess Score"]}</p>
+            <p className="card-text"><strong>Score Explanation</strong>
+              {item['score_explanation']}
+            </p>
             <p className="card-text"><strong>Hotel Name:</strong> {item["Summary"]['Hotel Name'] || 'No Hotel Name is Available'}</p>
             <p className="card-text"><strong>Permission:</strong> {item.Permission ? 'Granted' : 'Not Granted'}</p>
             <p className="card-text"><strong>Reason:</strong> {item.Reason || 'No reason provided'}</p>
             <p className="card-text"><strong>Relation:</strong> {item["Summary"]['Relation to the brand']}</p>
-            <p className="card-text"><strong>Inhouse/Wild:</strong> {item["Summary"].Inhouse_Wild ? 'Yes' : 'No'}</p>
+            <p className="card-text"><strong>Inhouse/Wild:</strong> {item["Summary"]['Inhouse/Wild']}</p>
+            <p className="card-text"><strong>Categories: </strong>
+              {item['Categories']}
+            </p>
 
             <p className="card-text"><strong>Hashtags:</strong> {item.Summary.Tags.join(', ') || 'No Hashtags are there'}</p>
-            <Link to={item.Result.url} target="_blank" rel="noopener noreferrer"  className="btn btn-primary">View on Instagram</Link>
+            <Link to={item.Result.url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">View on Instagram</Link>
           </div>
         </div>
       ))}
